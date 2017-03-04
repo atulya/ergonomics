9c026a09f0c15ec069f49ddde62569bc442a1de9
Atul Bhalerao <atul.geeky@gmail.com>
2017-03-04 11:51:20 +0530

    ergo02

---
+++ services.php
@@ -0,0 +1,245 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="apple-touch-icon" href="apple-touch-icon.png">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+  <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+</head>
+<body>
+  <!--[if lt IE 8]>
+  <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+  <![endif]-->
+  <?php include "menu.php" ?>
+  <!-- Main jumbotron for a primary marketing message or call to action -->
+  <div class="container content">
+    <div class="jumbotron banner servicesBanner">
+      <h1 class="title">Services</h1>
+    </div>
+    <!-- Example row of columns -->
+    <div class="row">
+      <div class="col-md-12 col-sm-12 col-xs-12">
+        <h3>MODULAR FURNITURE</h3>
+        <ul>
+          <li>
+            <strong>WORKSTATIONS :</strong>  Ergonomically designed workstations with perfect combination of look and feel, different heights, add on accessories utilizing the floor space in best possible way. Available in Partition based system and desking system includes linear workstations, L shape workstations.
+          </li>
+          <li>
+            <strong>MANAGER DESK :</strong>  Best suited options with Main Desk, side units and back storage.
+          </li>
+          <li>
+            <strong>CUBICLES :</strong>  Cubicles are solutions to build efficient and high performance workstations to have better privacy. These are normally used for Managers.
+          </li>
+          <li>
+            <strong>CONFERENCING :</strong> When it comes to discussions, Knowledge sharing, unanimous decisions then the best place is Board rooms and meeting rooms. Conferencing solutions gives options for group seating.
+          </li>
+          <li>
+            <strong>STORAGE :</strong>  Every workspace requires different types of storage. Personal  and for group. We can provide cabinets, credenza, pedestals and many more.
+          </li>
+          <li>
+            <strong>CAFETERIA TABLES :</strong> Office space is incomplete without breakout areas, cafeteria. We have options for this segment too.
+          </li>
+        </ul>
+        <h3> OFFICE CHAIRS :</h3>
+        <p>
+          Office chairs are just not the chairs. They carry image, comfort, style.  For better productivity and comfortable seating for long hours, the chair has to be ergonomically designed.  We support you to get best  seating solutions.
+        </p>
+        <h3>PARTITIONS :</h3>
+        <p>Partitions are to divide workspace in most efficient way, keeping in mind the esthetics and objectives of partitioning.</p>
+        <p><strong>Mainly Partitions can be made with</strong></p>
+        <ul>
+          <li>
+            Dry wall concept using different borads and framings.
+          </li>
+          <li>
+            Wooden partitions made with framing and Commercial plywood.
+          </li>
+          <li>
+            Aluminium Partitions  are made with Aluminium square pipes and Prelaminated boards.
+          </li>
+          <li>
+            Modular partitions is a growing concept and need of future. These partitions are made using aluminum standard sections, Glass and Laminated board. These can be easily dismantled and assembled. The concept is adoptability for change.
+          </li>
+        </ul>
+        <h3>FALSE CEILINGS :</h3>
+        <p>
+          All types of false ceiling adds to functionality and esthetics to space. It gives chance to use your various services like cabling, AC, Light fixtures in best possible way. Not only this, ceiling adds in to better  productivity and also saves on working cost of an office in long run. Different false ceiling solutions are available like plain ceiling, grid ceiling, metal ceiling ,open cell ceiling, baffle ceiling and designer ceiling.
+        </p>
+        <h3>OTHER PRODUCTS :</h3>
+        <p>
+          Office spaces do requires carpets, doors, windows, vanishing blinds, cladding options . Professional  Air conditioning  solutions, Electrical and networking  solutions are also need of any modern office. We have panel of right consultant to provide the technically correct solutions and at optimum price point.
+        </p>
+        <h3>INDUSTRIAL PROJECTS – INTERIORS AND EXTERIORS :</h3>
+        <p>
+          For Industrial projects,  one will surely need product options like
+          Pre engineered buildings, Mezzanine floors, Glass cladding, ACP cladding, High raised dry walls, industrial ceilings. All these work can be executed only with thee proper guidance of technical experts and skilled manpower. We got technical experts and execution teams in our panel.
+        </p>
+        <h3>RESIDENTIAL INTERIORS</h3>
+        <p>
+          We spend maximum time of our life at our sweet home. Its not only an address for us, but a place which is full of comfort and positivity. We all area attached emotionally with each corner and things in house.
+          To make it beautiful and most comfortable is an expert job. Your requirements needs to be understood well and accordingly to provide the best possible esthetics.
+        </p><p>
+        Designing and execution is our key strength. Our  Architect panel can guide u on design front and exection team can deliver the quality work. The quality material section is possible with their expertise at correct price points.
+      </p>
+      <h3>Services we are associated with :</h3>
+      <ul>
+        <li>
+          Chairs repairs and AMC
+        </li>
+        <li>
+          Furniture reconfiguration, rework and servicing
+        </li>
+        <li>
+          Electrical and networking consultancy and products, and services.
+        </li>
+        <li>
+          AC consultancy, services and AMC
+        </li>
+        <li>
+          Design and consultancy for Residential Interiors
+        </li>
+        <li>
+          Construction Material – consultancy and services
+        </li>
+        <li>
+          Carpentry Services
+        </li>
+        <li>
+          Painting -Internal and external
+        </li>
+      </ul>
+      <h3>Products we are associated with  :</h3>
+      <div class="col-md-6">
+        <ul class="row">
+          <li>Modular Furniture</li>
+          <li>Chairs and sofas</li>
+          <li>False ceiling</li>
+          <li>Glass partitions</li>
+          <li>Dry wall partitions</li>
+          <li>Raised flooring</li>
+          <li>Air conditioners</li>
+          <li>Light fittings</li>
+          <li>Steel storage system</li>
+          <li>Tile carpet/Role carpet</li>
+          <li>External Cladding</li>
+        </ul>
+      </div>
+      <div class="col-md-6">
+        <ul class="row">
+          <li>ACP and Glass facade</li>
+          <li>Material Handling equipments</li>
+          <li>Wooden Flooring</li>
+          <li>Railings</li>
+          <li>School furniture</li>
+          <li>Skirtings</li>
+          <li>Corner Guards</li>
+          <li>Vanishing blinds</li>
+          <li>Wall papers</li>
+          <li>Acoustic walls</li>
+          <li>Wall cladding</li>
+        </ul>
+      </div>
+    </div>
+    <div class="container hidden-xs hidden-sm hidden-md">
+      <div class="row">
+        <div class="col-md-8 height-280 footerUsp">
+          <a href="javascript:void(0);">
+            <img src="img/officeFooter.png" alt="Office">
+          </a>
+          <h3 class="footerUspTitle">Office</h3>
+        </div>
+        <div class="col-md-4 height-280 footerUsp">
+          <a href="javascript:void(0);">
+            <img src="img/industrialFooter.png" alt="Industrial Footer">
+          </a>
+          <h3 class="footerUspTitle">Industrial</h3>
+        </div>
+        <div class="col-md-4 height-280 footerUsp">
+          <a href="javascript:void(0);">
+            <img src="img/outdoorsFooter.png" alt="Outdoors Footer">
+          </a>
+          <h3 class="footerUspTitle">Outdoor</h3>
+        </div>
+        <div class="col-md-8 height-280 footerUsp">
+          <a href="javascript:void(0);">
+            <img src="img/residentialFooter.png" alt="Residential Footer">
+          </a>
+          <h3 class="footerUspTitle">Residential</h3>
+        </div>
+      </div>
+      </div> <!-- /container -->
+    </div>
+  </div>
+  <footer class="container-fluid" style="clear: both">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4"><h3>Services</h3>
+          <ul>
+            <li><a href="javascript:void(0);">Office</a></li>
+            <li><a href="javascript:void(0);">Commercial</a></li>
+            <li><a href="javascript:void(0);">Residential</a></li>
+            <li><a href="javascript:void(0);">Others</a></li>
+          </ul>
+        </div>
+        <div class="col-md-4"><h3>About Us</h3>
+          <ul>
+            <li><a href="javascript:void(0);">About us</a></li>
+            <li><a href="javascript:void(0);">Services</a></li>
+            <li><a href="javascript:void(0);">Contact us</a></li>
+            <li><a href="javascript:void(0);">Enquiry</a></li>
+          </ul>
+        </div>
+        <div class="col-md-4"><h3>Follow Us</h3>
+          <ul class="socialMedia">
+            <li>
+              <a href="javascript:void(0);">
+                <i class="fa fa-facebook-square"></i>
+              </a>
+            </li>
+            <li>
+              <a href="javascript:void(0);">
+                <i class="fa fa-twitter-square"></i>
+              </a>
+            </li>
+            <li>
+              <a href="javascript:void(0);">
+                <i class="fa fa-linkedin-square"></i>
+              </a>
+            </li>
+            <li>
+              <a href="javascript:void(0);">
+                <i class="fa fa-google-plus-square"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p class="text-center copyrights">All rights reserved, &copy; 2017</p>
+    </div>
+  </footer>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+  <script src="js/vendor/bootstrap.min.js"></script>
+  <script src="js/plugins.js"></script>
+  <script src="js/main.js"></script>
+  <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+  <script>
+  (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+  function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+  e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+  e.src='//www.google-analytics.com/analytics.js';
+  r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+  ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+  </script>
+</body>
+</html>
