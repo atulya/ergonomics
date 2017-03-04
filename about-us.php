d4c25d9bc6555062efe40acce93838a5683ac4b1
Atul Bhalerao <atul.geeky@gmail.com>
2017-03-04 11:51:20 +0530

    ergo02

---
+++ about-us.php
@@ -0,0 +1,202 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="apple-touch-icon" href="apple-touch-icon.png">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+  <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+</head>
+<body>
+  <!--[if lt IE 8]>
+  <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+  <![endif]-->
+<?php include "menu.php" ?>
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="container content">
+      <div class="jumbotron banner aboutusBanner">
+        <h1 class="title">About Us</h1>
+      </div>
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+          <h2>Commercial, Industrial, Residential Interiors products &amp; services</h2>
+          <p class="text-center"><strong> Connecting the clients to rite products and services in to Industrial ,commercial and residential Interiors. </strong></p>
+          <hr>
+          <p>
+            <strong>Key working Area :</strong>
+            <ul>
+              <li>Office and commercial Interiors </li>
+              <li>Industrial Interiors and exterior work</li>
+              <li>Residential Interiors</li>
+              <li>Design , execution and Project management service</li>
+              <li>Retail rework and maintenance services for chairs, furniture, carpets</li>
+            </ul>
+          </p>
+          <p>
+            We are team of professionals and experience people in industrial and commercial Interiors field, who are collectively offering the free consultancy to connect the clients with rite products and services in the industry.  We are networked with multiple quality products and services in the interior Industry. This platform is just not the networking of clients and products , but to help out the clients to reach the quality product and services at optimum price.
+          </p>
+          <hr>
+          <h3>Vision </h3>
+          <p>
+            <strong>Our Role</strong> – Interiors and ambiance are the essentials of every office, and industrial setup.
+            <br>
+            Offices and residence are built,  but creativity, functionality and looks are added with designer’s  thought process  and multiple interiors and exterior products and services incorporated in the design. The market is flooded with thousands of products in the interior industry.  Its tough time  to choose the correct products and creditable service providers in the industry.  <strong>We help you for</strong>
+            <ul>
+              <li>
+                Identifying the Interior products and services required for your office , commercial , Industrial or residential premises.
+              </li>
+              <li>
+                Understanding your exact requirements and expectations in terms of interior products and services.
+              </li>
+              <li>
+                Suggesting and connecting you to the rite company, right product and performer service provider with proper technical information free of cost.
+              </li>
+              <li>
+                Help you  to get the correct pricing for your Interiors products and services requirements.
+              </li>
+              <li>
+                Establishing the long term client –product-company-services relationship by offering you the best possible solutions till the completion of your job.
+              </li>
+            </ul>
+          </p>
+          <hr>
+          <h3>How it is possible with Ergonimics solutions .. ?? <br>( Guidance on operating process of website )</h3>
+          <ul>
+            <li>
+              You need to scan the website for your products and services requirement for office, commercial or Industrial Interiors.
+            </li>
+            <li>
+              Send us the enquiry of for the product and services of your interest through enquiry form.
+            </li>
+            <li>
+              Concern professional  of our team will reach to you for further detailing of your requirement.
+            </li>
+            <li>
+              The offer details of products and services will be given to you from the experts along with technical details.
+            </li>
+            <li>
+              You will be connected to the concern company for deal of products and/or services.
+            </li>
+          </ul>
+          <hr><h3>How this can make your life simple and tension free ??</h3>
+          <ul>
+            <li>
+              You are equipped with free technical guidance and information related to your multiple products and services  requirement at single point contact.
+            </li>
+            <li>
+              You will get the comparative offers giving scope to you for selection of products and services as per your exact technical and commercial expectations.
+            </li>
+            <li>
+              Professionals and experts in the industry are for help to give complete solutions.
+            </li>
+            <li>
+              You are accessible to Creditable, organized and technically sound service providers as per your needs.
+            </li>
+            <li>
+              Quality and satisfaction along with saved cost and time.
+            </li>
+          </ul>
+        </div>
+        <div class="container hidden-xs hidden-sm hidden-md">
+          <div class="row">
+            <div class="col-md-8 height-280 footerUsp">
+              <a href="javascript:void(0);">
+                <img src="img/officeFooter.png" alt="Office">
+              </a>
+              <h3 class="footerUspTitle">Office</h3>
+            </div>
+            <div class="col-md-4 height-280 footerUsp">
+              <a href="javascript:void(0);">
+                <img src="img/industrialFooter.png" alt="Industrial Footer">
+              </a>
+              <h3 class="footerUspTitle">Industrial</h3>
+            </div>
+            <div class="col-md-4 height-280 footerUsp">
+              <a href="javascript:void(0);">
+                <img src="img/outdoorsFooter.png" alt="Outdoors Footer">
+              </a>
+              <h3 class="footerUspTitle">Outdoor</h3>
+            </div>
+            <div class="col-md-8 height-280 footerUsp">
+              <a href="javascript:void(0);">
+                <img src="img/residentialFooter.png" alt="Residential Footer">
+              </a>
+              <h3 class="footerUspTitle">Residential</h3>
+            </div>
+          </div>
+          </div> <!-- /container -->
+        </div>
+      </div>
+      <footer class="container-fluid" style="clear: both">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4"><h3>Services</h3>
+              <ul>
+                <li><a href="javascript:void(0);">Office</a></li>
+                <li><a href="javascript:void(0);">Commercial</a></li>
+                <li><a href="javascript:void(0);">Residential</a></li>
+                <li><a href="javascript:void(0);">Others</a></li>
+              </ul>
+            </div>
+            <div class="col-md-4"><h3>About Us</h3>
+              <ul>
+                <li><a href="javascript:void(0);">About us</a></li>
+                <li><a href="javascript:void(0);">Services</a></li>
+                <li><a href="javascript:void(0);">Contact us</a></li>
+                <li><a href="javascript:void(0);">Enquiry</a></li>
+              </ul>
+            </div>
+            <div class="col-md-4"><h3>Follow Us</h3>
+              <ul class="socialMedia">
+                <li>
+                  <a href="javascript:void(0);">
+                    <i class="fa fa-facebook-square"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="javascript:void(0);">
+                    <i class="fa fa-twitter-square"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="javascript:void(0);">
+                    <i class="fa fa-linkedin-square"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="javascript:void(0);">
+                    <i class="fa fa-google-plus-square"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <hr />
+          <p class="text-center copyrights">All rights reserved, &copy; 2017</p>
+        </div>
+      </footer>
+      <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+      <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+      <script src="js/vendor/bootstrap.min.js"></script>
+      <script src="js/plugins.js"></script>
+      <script src="js/main.js"></script>
+      <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+      <script>
+      (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+      function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+      e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+      e.src='//www.google-analytics.com/analytics.js';
+      r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+      ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+      </script>
+    </body>
+  </html>
---
+++ office.php
@@ -0,0 +1,98 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="apple-touch-icon" href="apple-touch-icon.png">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+  <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+</head>
+<body>
+  <!--[if lt IE 8]>
+  <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+  <![endif]-->
+  <?php include "menu.php" ?>
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="container content">
+      <div class="jumbotron banner servicesBanner">
+        <h1 class="title">Office</h1>
+      </div>
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+          <h3 class="text-center">coming soon</h3>
+        </div>
+      </div>
+      </div>
+          <footer class="container-fluid" style="clear: both">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-4"><h3>Services</h3>
+                  <ul>
+                    <li><a href="javascript:void(0);">Office</a></li>
+                    <li><a href="javascript:void(0);">Commercial</a></li>
+                    <li><a href="javascript:void(0);">Residential</a></li>
+                    <li><a href="javascript:void(0);">Others</a></li>
+                  </ul>
+                </div>
+                <div class="col-md-4"><h3>About Us</h3>
+                  <ul>
+                    <li><a href="javascript:void(0);">About us</a></li>
+                    <li><a href="javascript:void(0);">Services</a></li>
+                    <li><a href="javascript:void(0);">Contact us</a></li>
+                    <li><a href="javascript:void(0);">Enquiry</a></li>
+                  </ul>
+                </div>
+                <div class="col-md-4"><h3>Follow Us</h3>
+                  <ul class="socialMedia">
+                    <li>
+                      <a href="javascript:void(0);">
+                        <i class="fa fa-facebook-square"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="javascript:void(0);">
+                        <i class="fa fa-twitter-square"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="javascript:void(0);">
+                        <i class="fa fa-linkedin-square"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="javascript:void(0);">
+                        <i class="fa fa-google-plus-square"></i>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <hr />
+              <p class="text-center copyrights">All rights reserved, &copy; 2017</p>
+            </div>
+          </footer>
+          <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+          <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+          <script src="js/vendor/bootstrap.min.js"></script>
+          <script src="js/plugins.js"></script>
+          <script src="js/main.js"></script>
+          <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+          <script>
+          (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+          function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+          e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+          e.src='//www.google-analytics.com/analytics.js';
+          r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+          ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+          </script>
+        </body>
+      </html>
